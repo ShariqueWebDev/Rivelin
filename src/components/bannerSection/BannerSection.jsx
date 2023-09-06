36d9a5c01749b5a6d69192e76877283863972085
sharique shaikh <shariquedev966@gmail.com>
2023-09-06 22:24:57 +0530

    third commit

--- src/components/bannerSection/BannerSection.jsx
+++ src/components/bannerSection/BannerSection.jsx
@@ -27,48 +27,7 @@ const BannerSection = () => {
   return (
     <ContentWrapper>
       <div className="main_container">
-        <div className="intro">
-          <div className="logo">
-            <img src={logo} alt="" />
-          </div>
-          <div className="text">
-            <h1>Hospitality Solution</h1>
-            <p>where hospitality meets the bay</p>
-          </div>
-        </div>
-        <div className="summary_text">
-          <h1>WELCOME TO Rivelin TRADING</h1>
-          <p>
-            Rivelin General Trading has been established with the intent of
-            providing hotels, furnished apartments, and others in the
-            hospitality industry a reliable source for all their supply and
-            equipment needs. Our business model is based on providing effective
-            and efficient solutions for each project in industry.
-            <br />
-            <br />
-          </p>
-          <div
-            className={`more_content ${
-              readMore ? "active_read" : "deactive_read"
-            }`}
-          >
-            Our vision is to be A One-Stop-Shop for everyone setting up new
-            hotels and restaurants, anyone streamlining processes in hotels and
-            for those contemplating a makeover of existing hotels and
-            restaurants. Our comprehensive range of hotel supplies is ideal for
-            hotel and guestroom properties, including essential toiletries,
-            amenities and furniture all designed with hospitality in mind.
-          </div>
-          <button
-            className="read_btn"
-            onClick={() => {
-              setReadMore(!readMore);
-            }}
-          >
-            {readMore ? "Hide Text" : "Read More"}
-          </button>
-        </div>
-        <div className="banner_img">
+      <div className="banner_img">
           <Carousel
            autoPlay={true}
            infiniteLoop={true}
@@ -97,6 +56,47 @@ const BannerSection = () => {
             }
           </Carousel>
         </div>
+        <div className="summary_text">
+          <h1>WELCOME TO Rivelin TRADING</h1>
+          <p>
+            Rivelin General Trading has been established with the intent of
+            providing hotels, furnished apartments, and others in the
+            hospitality industry a reliable source for all their supply and
+            equipment needs. Our business model is based on providing effective
+            and efficient solutions for each project in industry.
+            <br />
+            <br />
+          </p>
+          <div
+            className={`more_content ${readMore ? "active_read" : "deactive_read"}`}
+          >
+            Our vision is to be A One-Stop-Shop for everyone setting up new
+            hotels and restaurants, anyone streamlining processes in hotels and
+            for those contemplating a makeover of existing hotels and
+            restaurants. Our comprehensive range of hotel supplies is ideal for
+            hotel and guestroom properties, including essential toiletries,
+            amenities and furniture all designed with hospitality in mind.
+          </div>
+          <button
+            className="read_btn"
+            onClick={() => {
+              setReadMore(!readMore);
+            }}
+          >
+            {readMore ? "Hide Text" : "Read More"}
+          </button>
+        </div>
+        <div className="intro">
+          <div className="logo">
+            <img src={logo} alt="" />
+          </div>
+          <div className="text">
+            <h1>Hospitality Solution</h1>
+            <p>where hospitality meets the bay</p>
+          </div>
+        </div>
+       
+        
       </div>
     </ContentWrapper>
   );
