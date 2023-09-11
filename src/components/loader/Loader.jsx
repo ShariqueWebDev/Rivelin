bf399ee7ef3b0ffd86c6afaeb1577c5dc07dfca4
sharique shaikh <shariquedev966@gmail.com>
2023-09-11 18:50:35 +0530

    change loader

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./Loader.scss"
 import spinner from "../../assets/spinner.gif";
+import {ColorRing} from "react-loader-spinner"
 
 const Loader = () => {
   return (
     <div>
       <div className="spinner">
-        <img src={spinner} alt="" />
+        <div className="img">
+          <ColorRing/>
+        </div>
         <div className="spinner_layer"></div>
       </div>
     </div>
